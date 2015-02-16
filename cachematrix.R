f43651724ccb9695280cb1b3582f90bbb897fe66
felix <felix@firefly>
2015-02-16 15:50:00 +0100

    first try, seems to work :)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that allow cached computation of inverse matrices
+## Note: input matrices are assumed to be invertible
 
-## Write a short comment describing this function
+# Example Usage:
+#
+# > B <- matrix(c(2,2,3,2), ncol=2, nrow=2)
+# > x <- makeCacheMatrix(B)
+# cacheSolve(x)
+#
 
+# creates a cachematrix for an input matrix x. The result
+# of this function can then be passed to cacheSolve() in order
+# to compute the inverse of x such that repeated invocations
+# use a cached result.
 makeCacheMatrix <- function(x = matrix()) {
+
+	# this is the cached result
+	inverse <- NULL
+	
+	# set the input matrix (this can be used to reuse the vector returned
+	# by makeCacheMatrix for a different input matrix
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL #make sure to clear cached result
+	}
+	
+	# returns the input matrix
+	get <- function() x
+	
+	# set the inverse matrix.
+	setinverse <- function(i) inverse <<- i
+	getinverse <- function() inverse
+	list(set = set, get = get,
+			setinverse = setinverse,
+			getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
+# returns the inverse of a matrix x. x
+# must be a cachematrix obtained via makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+	# did we compute the result before?
+	result <- x$getinverse()
+	
+	if (is.null(result)){
+		# inverse has not been computed before, so compute it now
+		matrix <- x$get() # this is the original input matrix
+		result <- solve(matrix) # solve it
+		x$setinverse(result) # cache the result in the cache matrix
+	}
+
+	result # return the result
 }
